cb6f35042801da79165c54c0fc0691ba8f04fa7c
Dmytry Bondariev <bondarievmd@gmail.com>
2023-09-08 21:12:31 +0300

    Added first variant of tender list view; added and changed get method in index view

--- tenders/views.py
+++ tenders/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
+from django.db.models import Sum
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
+from django.views import generic
+
+from tenders.models import Tender
 
 
 class IndexView(LoginView):
@@ -7,5 +13,22 @@ class IndexView(LoginView):
     form_class = UserLoginForm
     redirect_authenticated_user = True
 
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect("tenders:tender-list")
+        return super().get(request, *args, **kwargs)
+
     def get_success_url(self):
         return reverse_lazy("tenders:tender-list")
+
+
+class TenderListView(generic.ListView, LoginRequiredMixin):
+    model = Tender
+    paginate_by = 10
+    template_name = "pages/tender_list.html"
+
+    def get_queryset(self):
+        tenders = Tender.objects.all()
+        total_tender_amount = Tender.objects.aggregate(total_amount=Sum('amount'))['total_amount']
+        return tenders, total_tender_amount
+
