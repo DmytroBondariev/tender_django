1c1add316e905239f3b9c4ad24a94ada2ad1da3f
Dmytry Bondariev <bondarievmd@gmail.com>
2023-09-08 20:44:14 +0300

    Added first variant of index view(home page)

--- tender_django/settings.py
+++ tender_django/settings.py
@@ -105,6 +105,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
+AUTH_USER_MODEL = "tenders.User"
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Ukraine/Kiev'
--- tenders/views.py
+++ tenders/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class IndexView(LoginView):
+    template_name = "pages/index.html"
+    form_class = UserLoginForm
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy("tenders:tender-list")
