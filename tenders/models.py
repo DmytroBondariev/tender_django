d2249055af51b3d1039f732accdf3eca8d8a5840
Dmytry Bondariev <bondarievmd@gmail.com>
2023-09-08 19:23:05 +0300

    Added tender model

--- tenders/models.py
+++ tenders/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Tender(models.Model):
+    tender_id = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date_modified = models.DateTimeField()
+
+    class Meta:
+        ordering = ["-date_modified"]
+        verbose_name = "Tender"
+        verbose_name_plural = "Tenders"
+
+    def __str__(self):
+        return f"Tender id: {self.tender_id}; ({self.description}, {self.amount}). Date modified: {self.date_modified}"
+
+    def get_absolute_url(self):
+        return reverse("tenders:tender-detail", kwargs={"pk": self.id})
+
+
+
+
+
