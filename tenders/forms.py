191760dacf867f009e956e08dfc8ee5e75bfa309
Dmytry Bondariev <bondarievmd@gmail.com>
2023-09-09 17:37:20 +0300

    Added user creation form

--- tenders/forms.py
+++ tenders/forms.py
@@ -1,20 +1,10 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UsernameField
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.forms import AuthenticationForm, UsernameField, UserCreationForm
 
 
-# class UserLoginForm(AuthenticationForm):
-#     username = UsernameField(
-#         widget=forms.TextInput(
-#             attrs={"class": "form-control form-control-lg",
-#                    "placeholder": "Username"}
-#         )
-#     )
-#     password = forms.CharField(
-#         label=_("Password"),
-#         strip=False,
-#         widget=forms.PasswordInput(
-#             attrs={"class": "form-control form-control-lg",
-#                    "placeholder": "Password"}
-#         ),
-#     )
+from tenders.models import User
+
+
+class UserCreateForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields
