6126eb4f4cfac0866461aa3bf6b349e3f3416680
Dmytry Bondariev <bondarievmd@gmail.com>
2023-09-09 14:22:15 +0300

    Added tender list url

--- tenders/urls.py
+++ tenders/urls.py
@@ -7,6 +7,7 @@ from tenders.views import (
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("tenders/", TenderListView.as_view(), name="tender-list"),
     path("logout/", views.LogoutView.as_view(), name="logout")
 ]
 
