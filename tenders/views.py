f5c30d17c6072417b62a63b4e5941e2d2bbf7337
Dmytry Bondariev <bondarievmd@gmail.com>
2023-09-09 14:24:08 +0300

    Removed unnecessary methods overwriting in index view; overwrote login url for login required mixin in tender list view

--- tenders/views.py
+++ tenders/views.py
@@ -2,11 +2,9 @@ import requests
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.db.models import Sum
-from django.shortcuts import redirect
-from django.urls import reverse_lazy
 from django.views import generic
 
-from tenders.forms import UserLoginForm
+
 from tenders.models import Tender
 
 api_url = 'https://public.api.openprocurement.org/api/0/tenders?descending=1'
@@ -26,21 +24,14 @@ class IndexView(LoginView):
     template_name = "pages/index.html"
     redirect_authenticated_user = True
 
-    def get(self, request, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            return redirect("tenders:tender-list")
-        return super().get(request, *args, **kwargs)
 
-    def get_success_url(self):
-        return reverse_lazy("tenders:tender-list")
-
-
-class TenderListView(generic.ListView, LoginRequiredMixin):
+class TenderListView(LoginRequiredMixin, generic.ListView):
     model = Tender
     paginate_by = 10
     template_name = "pages/tender_list.html"
+    login_url = "/"
 
     def get_queryset(self):
         tenders = Tender.objects.all()
-        total_tender_amount = Tender.objects.aggregate(total_amount=Sum('amount'))['total_amount']
-        return tenders, total_tender_amount
+        # total_tender_amount = Tender.objects.aggregate(total_amount=Sum('amount'))['total_amount']
+        return tenders
