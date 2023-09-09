e89aba87872a1ef9fd7382d7084395d67a68b734
Dmytry Bondariev <bondarievmd@gmail.com>
2023-09-09 14:57:53 +0300

    Removed unnecessary login form; changed links order in sidebar

--- tenders/forms.py
+++ tenders/forms.py
@@ -3,18 +3,18 @@ from django.contrib.auth.forms import AuthenticationForm, UsernameField
 from django.utils.translation import gettext_lazy as _
 
 
-class UserLoginForm(AuthenticationForm):
-    username = UsernameField(
-        widget=forms.TextInput(
-            attrs={"class": "form-control form-control-lg",
-                   "placeholder": "Username"}
-        )
-    )
-    password = forms.CharField(
-        label=_("Password"),
-        strip=False,
-        widget=forms.PasswordInput(
-            attrs={"class": "form-control form-control-lg",
-                   "placeholder": "Password"}
-        ),
-    )
+# class UserLoginForm(AuthenticationForm):
+#     username = UsernameField(
+#         widget=forms.TextInput(
+#             attrs={"class": "form-control form-control-lg",
+#                    "placeholder": "Username"}
+#         )
+#     )
+#     password = forms.CharField(
+#         label=_("Password"),
+#         strip=False,
+#         widget=forms.PasswordInput(
+#             attrs={"class": "form-control form-control-lg",
+#                    "placeholder": "Password"}
+#         ),
+#     )
