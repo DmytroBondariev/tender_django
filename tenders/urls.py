937b10ef835b5959c9ddbe1466cc9317f4659eb6
Dmytry Bondariev <bondarievmd@gmail.com>
2023-09-10 10:40:44 +0300

    Added corresponding url; created corresponding post form on tender list page; configured redirect to tender list page

--- tenders/urls.py
+++ tenders/urls.py
@@ -2,14 +2,15 @@ from django.contrib.auth import views
 from django.urls import path
 from tenders.views import (
     IndexView,
-    TenderListView, RegisterView,
+    TenderListView, RegisterView, ButtonToGetTenderView,
 )
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("register/", RegisterView.as_view(), name="register"),
     path("tenders/", TenderListView.as_view(), name="tender-list"),
-    path("logout/", views.LogoutView.as_view(), name="logout")
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("tenders/update/", ButtonToGetTenderView.as_view(), name="tender-update-button"),
 ]
 
 app_name = "tenders"
--- tenders/views.py
+++ tenders/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model, forms
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.db.models import Sum
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -10,16 +11,6 @@ from tenders.forms import UserCreateForm
 from tenders.models import Tender, User
 
 API_URL = 'https://public.api.openprocurement.org/api/0/tenders?descending=1'
-# response = requests.get(api_url)
-# data = response.json()
-# for item in data['data'][:10]:
-#     if Tender.objects.filter(tender_id=item['id']).exists():
-#         continue
-#     tender = Tender(
-#         tender_id=item['id'],
-#         date_modified=item['dateModified']
-#     )
-#     tender.save()
 
 
 class RegisterView(generic.CreateView):
@@ -58,19 +49,6 @@ class TenderListView(LoginRequiredMixin, generic.ListView):
         return tenders
 
 
-# class ToggleAssignToTaskView(generic.View):
-#     @staticmethod
-#     def post(request, pk):
-#         worker = Worker.objects.get(id=request.user.id)
-#         task = Task.objects.get(id=pk)
-#         if task in worker.tasks.all():
-#             worker.tasks.remove(task)
-#         else:
-#             worker.tasks.add(task)
-#         return HttpResponseRedirect(
-#             reverse_lazy("task_manager:task-detail", args=[pk])
-#         )
-
 class ButtonToGetTenderView(generic.View):
     @staticmethod
     def post(request):
@@ -84,3 +62,6 @@ class ButtonToGetTenderView(generic.View):
                 date_modified=item['dateModified']
             )
             tender.save()
+        return HttpResponseRedirect(
+            reverse_lazy("tenders:tender-list")
+        )
