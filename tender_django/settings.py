2cd67ea431a04f497ec35e554f450edf0d3e3d5d
Dmytry Bondariev <bondarievmd@gmail.com>
2023-09-10 21:22:37 +0300

    Added .env and .env.sample files and moved Django secret key to .env file

--- tender_django/settings.py
+++ tender_django/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-275ttp+j=a3-@34_v_$wsk&ua&kyhsqj(et)+c&uxv2b6^ych6'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
